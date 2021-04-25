c7b082563dd495550ac34d8b48e6025929463d24
aryan2090 <aryanrajeshsharma@gmail.com>
2021-04-25 17:09:54 +0530

    .......

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from "react-dom";
 import "./styles/main.css";
 import "./styles/test.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 import ReactGA from 'react-ga';
 const TRACKING_ID = process.env.REACT_APP_TRACKING_ID;
 ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById("root")
 );
-reportWebVitals();
